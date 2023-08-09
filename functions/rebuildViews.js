9a5034014bfe5cf300c8ce12a94671ca44523902
Zac McWilliam <zac.mcwilliam@robocupjunior.org.au>
2023-08-09 18:03:15 +1000

    RC-645 - Run PHP view files on rebuildViews

--- functions/rebuildViews.js
+++ functions/rebuildViews.js
@@ -49,6 +49,46 @@ async function rebuildViews(deploymentInfo) {
                 });
             });
         }
+
+        const runViewPHP = async (database_name, deploymentInfo) => {
+            return new Promise((resolve, reject) => {
+                const viewCmd = spawn(process.env.PHP_PATH, [
+                    "utils/db_files/2.5_views_script_alt.php",
+                    database_name
+                ], {
+                    cwd: deploymentInfo.path
+                });
+                
+                viewCmd.on('exit', (code) => {
+                    if (code === 0) {
+                        console.log(`[REBUILDVIEW] PHP Migration for ${database_name} complete`);
+                        rebuildViewsLog += `\n[REBUILDVIEW] PHP Migration for ${database_name} complete`;
+                        resolve();
+                    } else {
+                        console.error(`[REBUILDVIEW] Error running PHP view file on ${database_name}`);
+                        rebuildViewsLog += `\n[REBUILDVIEW] Error running PHP view file on ${database_name}`;
+                        reject(code);
+                    }
+                });
+                
+                viewCmd.on('error', (err) => {
+                    console.error(`[REBUILDVIEW] Error running PHP view file on ${database_name}`);
+                    rebuildViewsLog += `\n[REBUILDVIEW] Error running PHP view file on ${database_name}`;
+                    reject(err);
+                });
+                
+                viewCmd.stdout.on('data', (data) => {
+                    console.log(data.toString());
+                    rebuildViewsLog += data;
+                });
+                
+                viewCmd.stderr.on('data', (data) => {
+                    console.log(data.toString());
+                    rebuildViewsLog += data;
+                });
+            });
+        }
+
         // Connect to the MariaDB server
         const pool = mariadb.createPool({
             host: process.env.DB_HOST,
@@ -65,15 +105,24 @@ async function rebuildViews(deploymentInfo) {
         for (const compDb of compDatabases) {
             const dbName = compDb.SCHEMA_NAME;
             
-            console.log(`[REBUILDVIEW] Rebuilding views in ${dbName}`);
-            rebuildViewsLog += `\n[REBUILDVIEW] Rebuilding views in ${dbName}`;
+            console.log(`[REBUILDVIEW] Rebuilding SQL views in ${dbName}`);
+            rebuildViewsLog += `\n[REBUILDVIEW] Rebuilding SQL views in ${dbName}`;
 
             // Connect to the comp database
             await conn.query(`USE ${dbName}`);
 
             await runViewSQL(dbName, deploymentInfo).catch((err) => {
-                console.error('[REBUILDVIEW] Error running view file:', err?.message || err);
-                rebuildViewsLog += '\n[REBUILDVIEW] Error running view file: ' + (err?.message || err);
+                console.error('[REBUILDVIEW] Error running SQL view file:', err?.message || err);
+                rebuildViewsLog += '\n[REBUILDVIEW] Error running SQL view file: ' + (err?.message || err);
+                rebuildViewsFailed = true;
+            });
+
+            console.log(`[REBUILDVIEW] Rebuilding SQL views in ${dbName} complete`);
+            rebuildViewsLog += `\n[REBUILDVIEW] Rebuilding SQL views in ${dbName} complete`;
+
+            await runViewPHP(dbName, deploymentInfo).catch((err) => {
+                console.error('[REBUILDVIEW] Error running PHP view file:', err?.message || err);
+                rebuildViewsLog += '\n[REBUILDVIEW] Error running PHP view file: ' + (err?.message || err);
                 rebuildViewsFailed = true;
             });
 
