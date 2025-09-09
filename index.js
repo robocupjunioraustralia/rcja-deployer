bb919a3c2eb21f42dfddc1cb9d2c0f753af87add
Zac McWilliam <zac.mcwilliam@robocupjunior.org.au>
2025-09-09 21:17:59 +1000

    RC-1306 - Fix deployment reference

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/deploy', async (req, res) => {
     const [migrateFailed, migrateLog] = await runDatabaseMigrations(
       selected_deployment,
       !selected_deployment.backup,
-      toDeployment.no_composer_dev || false
+      selected_deployment.no_composer_dev || false
     );
     console.log("[DEPLOY] Migration complete: ", migrateFailed ? "FAIL" : "SUCCESS");
     deployLog += migrateLog;
